6618d857f8b405bf56d66dc14f91ca829986e585
Hasin Hayder <ironhide@leevio.com>
2013-02-02 18:08:09 +0600

    JS and Constants Fixed

---
+++ home.js.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-type:text/javascript");
+include_once("config.php");
+?>
+$(document).ready(function(){
+  $("#first").remove();
+  $("#bit").remove();
+  $("#commentform").remove();
+  $("#comments").html('<div id="fb-root"></div><div class="fb-comments" data-href="'+location.href+'" data-width="<?php echo FACEBOOK_WIDTH;?>" data-num-posts="20"></div>');
+  var js, fjs = document.getElementsByTagName("script")[0];
+  if (document.getElementById("facebook-jssdk")) return;
+  js = document.createElement("script"); js.id = "facebook-jssdk";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?php echo FACEBOOK_APP_ID;?>";
+  fjs.parentNode.insertBefore(js, fjs);
+});
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-$dataurl =$primaryurl = "http://ph0t0graphs.wordpress.com";//old domain
-$secondaryurl = "http://wpproxy.rs.af.cm"; //new domain
+include_once("config.php");
+$dataurl =$primaryurl = OLD_BLOG_URL;
+$secondaryurl = NEW_BLOG_URL;
 $path =array_keys($_GET);
 if(!empty($path[0])) $dataurl = "{$primaryurl}/{$path[0]}";
 $data = file_get_contents($dataurl);
@@ -9,10 +10,13 @@ $data = preg_replace($pattern,"{$secondaryurl}/$1",$data);
 $data = str_replace(
   array(
     "<a href=\"{$primaryurl}",
-    "</body>"),
+    "</body>",
+    "<meta"
+    ),
   array(
     "<a href=\"{$secondaryurl}",
-    "<script src='http://code.jquery.com/jquery.min.js'></script><script src='{$secondaryurl}/home.js'></script></body>"),$data);
-// $data = str_replace(array("<a href=\"{$primaryurl}","<form action=\"{$secondaryurl}"),array("<a href=\"{$secondaryurl}","<form action=\"{$primaryurl}"),$data);
+    "<script src='http://code.jquery.com/jquery.min.js'></script><script src='{$secondaryurl}/home.php'></script></body>",
+    "<meta property='fb:admins' content='".FACEBOOK_ID."'/><meta property='fb:app_id' content='".FACEBOOK_APP_ID."'/><meta"
+    ),$data);
 echo $data;
 ?>
