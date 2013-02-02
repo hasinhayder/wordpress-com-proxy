d976ee4252d872c8fea4130f55eecf572fd69cd6
Hasin Hayder <ironhide@leevio.com>
2013-02-02 18:25:27 +0600

    Minor Fixes

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("OLD_BLOG_URL","http://ph0t0graphs.wordpress.com/"); //no trailing slash, please
+define("OLD_BLOG_URL","http://ph0t0graphs.wordpress.com"); //no trailing slash, please
 define("NEW_BLOG_URL","http://wpproxy.rs.af.cm"); //no trailing slash, please
-define("FACEBOOK_ID","503274632"); //no trailing slash, please
+define("FACEBOOK_ID","503274632"); 
 define("FACEBOOK_APP_ID","449469471758092"); //no trailing slash, please
 define("FACEBOOK_WIDTH","700"); //no trailing slash, please
