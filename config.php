98b337c9a52cf0c5dfb0a5b63faf2d3cd16a874f
Hasin Hayder <ironhide@leevio.com>
2013-02-03 00:05:07 +0600

    Config Changed

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("OLD_BLOG_URL","http://hasin.me"); //your wordpress.com blog url with no trailing slash
+define("OLD_BLOG_URL","http://hasin.wordpress.com"); //your wordpress.com blog url with no trailing slash
 define("NEW_BLOG_URL","http://wpproxy.rs.af.cm"); //url from where you are hosting this script
 define("FACEBOOK_ID",""); //add your facebook id if you want to moderate comments
 define("FACEBOOK_APP_ID","249643311490"); //your facebook app id, default is the id of wordpress app
